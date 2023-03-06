792d895e7cf2cb47e912796b287109f9205152b5
Valdo <gkorchun@gmail.com>
2023-03-06 17:32:38 +0200

    Dabbled with useReducer

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './internal-tools/use-state/app';
+import App from './internal-tools/use-reducer/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

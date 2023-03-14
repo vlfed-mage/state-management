92268f501c07fd84c3513238c472a868b69a65ee
Valdo <gkorchun@gmail.com>
2023-03-14 11:38:09 +0200

    Publish and use npm package

--- src/custom-hooks/building-network-request-hook/use-picture/use-picture.js
+++ src/custom-hooks/building-network-request-hook/use-picture/use-picture.js
@@ -1,4 +1,4 @@
-import useNetwork from '../use-network';
+import { useNetwork } from 'servises-hooks'; // import from node-modules after publishing package to npm
 
 const usePicture = day => {
     const api_key = 'Nnn33kCAlDldcGYBaBUPq6gRe1OuMWRuaAPd7p3f';
